d8dd83c3f5125b3a21e6800b37708b25b32926b5
ElmerKainos <123182590+ElmerKainos@users.noreply.github.com>
2024-02-27 09:53:25 +0000

    dd

--- app/routes.js
+++ app/routes.js
@@ -7,3 +7,67 @@ const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
 // Add your routes here
+
+
+
+router.post ('/has-account', (req,res) => {
+if (req.session.data.new.hasAccount == 'No') {
+      res.redirect('/can-apply')
+
+} else {
+
+
+ res.redirect('/email')
+
+
+
+}
+
+
+})
+
+router.post ('/can-apply', (req,res) => {
+    if (req.session.data.new.canApply == 'No') {
+          res.redirect('/noteligible')
+    
+    } else {
+    
+    
+     res.redirect('/email')
+    
+    
+    
+    }
+    
+    
+    })
+router.post ('/email', (req,res) => {
+      res.redirect('/code')
+      
+      
+      
+ })
+router.post ('/code', (req,res) => {
+            req.session.data.user ={  }
+            res.redirect('/application')
+            
+            
+            
+ })
+
+ router.get ('/account/sign-out', (req,res) => {
+     delete  req.session.data.user
+      res.redirect('/has-account')
+      
+      
+      
+})
+
+
+
+
+
+
+require('./routes/application/show')(router)
+        
+
